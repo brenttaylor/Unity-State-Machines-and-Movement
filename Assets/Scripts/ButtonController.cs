6b393788dd916cb291d77db8cf61f0fafeb4e5e6
Brent Taylor <btaylor@fuzzylogicstudios.com>
2021-02-24 20:08:37 -0800

    Initial Commit

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+    [SerializeReference] private GameObject leftButton;
+    [SerializeReference] private GameObject rightButton;
+    void Update() {
+        leftButton.SetActive(false);
+        rightButton.SetActive(false);
+
+        try {
+            if (Input.GetButton("Left")) {
+                leftButton.SetActive(true);
+            }
+
+            if (Input.GetButton("Right")) {
+                rightButton.SetActive(true);
+            }
+        }
+        catch (ArgumentException) {
+        }
+    }
+}
