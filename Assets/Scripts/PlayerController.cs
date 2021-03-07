c0a7e1722a916bd7c5964b46b28b2fd43ed25d08
Brent Taylor <btaylor@fuzzylogicstudios.com>
2021-03-06 20:52:22 -0800

    Final Build

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,30 +2,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Bolt;
 
 public class PlayerController : MonoBehaviour {
     [SerializeField] private float metersPerSecond = 10.0f;
 
     private Rigidbody2D rigidBody;
-    private bool moveLeft = false;
-    private bool moveRight = false;
+
+    public enum States {
+        IDLE,
+        MOVING_LEFT,
+        MOVING_RIGHT
+    }
+
+    public enum Events {
+        LEFT_DOWN,
+        LEFT_UP,
+        RIGHT_DOWN,
+        RIGHT_UP
+    }
+
+    private States currentState {
+        get {
+            return (States) Variables.Object(gameObject).Get("currentState");
+        }
+    }
 
     void Start() {
         rigidBody = GetComponent<Rigidbody2D>();
     }
 
     void Update() {
-        moveLeft = Input.GetButton("Left");
-        moveRight = Input.GetButton("Right");
+        if (Input.GetButtonDown("Left")) TriggerEvent(Events.LEFT_DOWN);
+        if (Input.GetButtonDown("Right")) TriggerEvent(Events.RIGHT_DOWN);
+        if (Input.GetButtonUp("Left")) TriggerEvent(Events.LEFT_UP);
+        if (Input.GetButtonUp("Right")) TriggerEvent(Events.RIGHT_UP);
     }
 
     private void FixedUpdate() {
         float velocity = metersPerSecond * Time.deltaTime;
         Vector2 position = transform.position;
 
-        if (moveLeft) position.x -= velocity;
-        if (moveRight) position.x += velocity;
+        switch (currentState) {
+            case States.MOVING_LEFT:
+                position.x -= velocity;
+                break;
+            case States.MOVING_RIGHT:
+                position.x += velocity;
+                break;
+            case States.IDLE:
+                return;
+        }
         
         rigidBody.MovePosition(position);
+    }
+    
+    private void TriggerEvent(Events evt) {
+                CustomEvent.Trigger(this.gameObject, evt.ToString());
     }
 }
